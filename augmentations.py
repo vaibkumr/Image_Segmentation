749468c17281eb07d47094accd7d8fadf74000e8
timetraveller_san <anonymous.yolo.wtf@gmail.com>
2019-11-09 23:30:29 +0000

    aug fix

--- augmentations.py
+++ augmentations.py
@@ -5,8 +5,8 @@ def get_training_augmentation():
         # albu.Resize(320, 480),
         # albu.Resize(350, 525),
         albu.CLAHE(p=1),
-        albu.HorizontalFlip(p=0.25),
-        albu.VerticalFlip(p=0.25),
+        albu.HorizontalFlip(),
+        albu.VerticalFlip(),
         albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1,
                                 p=0.5, border_mode=0),
         # albu.GridDistortion(p=0.5),
@@ -20,6 +20,8 @@ def get_validation_augmentation():
     test_transform = [
         # albu.Resize(320, 480),
         # albu.Resize(350, 525),
+        albu.HorizontalFlip(),
+        albu.VerticalFlip(),
         albu.CLAHE(p=1),
     ]
     return albu.Compose(test_transform)
